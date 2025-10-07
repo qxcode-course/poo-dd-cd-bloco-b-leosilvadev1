188ddce706cd069ed5c285b25d813f8a5ddaec7f
Leandro silva <leandroslima09@gmail.com>
2025-10-07 17:11:06 -0300

    chinela completa

---
+++ myrep/poo/chinela/py/draft.py
@@ -0,0 +1,21 @@
+class Chinela:
+    def __init__(self):
+        self.__tamanho = 0
+
+    def setTamanho(self, valor: int) -> None:
+        if valor % 2 == 0 and 20 <= valor <= 50:
+            self.__tamanho = valor
+            return self.__tamanho
+
+    def getTamanho(self): # métodos em python tem self como primeiro atributo
+        return self.__tamanho
+
+def main():
+    chinela = Chinela()
+    while chinela.getTamanho() == 0: # mantendo usuário no loop
+        print("Digite seu tamanho de chinela")
+        tamanho = int(input()) # lendo a resposta e convertendo pra inteiro
+        chinela.setTamanho(tamanho)
+        print("Parabens, você comprou uma chinela tamanho", chinela.getTamanho())
+
+main()
